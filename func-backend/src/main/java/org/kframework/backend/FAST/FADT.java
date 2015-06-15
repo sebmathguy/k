66aa5ce295215decc19b6418cb87f4b7488d75dc
Sebastian Conybeare <sebmathguy@gmail.com>
2015-06-15 15:42:32 -0500

    Fix circular references in ADTs

--- func-backend/src/main/java/org/kframework/backend/FAST/FADT.java
+++ func-backend/src/main/java/org/kframework/backend/FAST/FADT.java
@@ -2,40 +2,19 @@
 package org.kframework.backend.FAST;
 
 import com.google.common.collect.ImmutableList;
-
-import java.util.stream.Collectors;
-
 /**
  * @author: Sebastian Conybeare
  */
-public class FADT extends FDeclarable {
+public abstract class FADT extends FDeclarable {
 
-    private final ImmutableList<FConstructor> constructors;
-    private final FTypeVar type;
-
-    public FADT(FTarget target, ImmutableList<FArgumentSignature> argSigs) {
+    protected FADT(FTarget target) {
         super(target);
-        type = new FTypeVar(target);
-        constructors = ImmutableList.copyOf(
-            argSigs.stream()
-            .map(argSig -> new FConstructorSignature(argSig, type))
-            .map(conSig -> new FConstructor(conSig, target))
-            .collect(Collectors.toList())
-            );
-        
-
     }
 
-    public ImmutableList<FConstructor> getFConstructors() {
-        return constructors;
-    }
+    public abstract ImmutableList<FConstructor> getFConstructors();
 
-    public FTypeVar getTypeVar() {
-        return type;
-    }
+    public abstract FTypeVar getTypeVar();
 
-    @Override
-    public void declare() {
-    }
-    
+    public abstract void declare();
+
 }
